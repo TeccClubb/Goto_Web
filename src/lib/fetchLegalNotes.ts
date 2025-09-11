81215fa1ea1e18236b49a737caf94d710925e7f3
M Saqib Ali <MSaqibAliISF@gmail.com>
2025-09-11 15:31:23 +0500

    fix: handle errors more gracefully in fetchLegalNotes function

--- src/lib/fetchLegalNotes.ts
+++ src/lib/fetchLegalNotes.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosError } from "axios";
 import { GET_LEGAL_NOTES_ROUTE } from "./constants";
 
 type LegalNotes = {
@@ -12,32 +11,27 @@ export const fetchLegalNotes = async (): Promise<{
   legalNotes?: LegalNotes;
 }> => {
   try {
-    const res = await axios.get<LegalNotes>(GET_LEGAL_NOTES_ROUTE, {
+    const res = await fetch(GET_LEGAL_NOTES_ROUTE, {
       headers: {
         Accept: "application/json",
-        "Cache-Control": "no-cache", // Indicates that the response can be stored but must be revalidated before reuse.
-        Pragma: "no-cache", // A legacy header, but still useful for older HTTP/1.0 caches.
-        Expires: "0", // Instructs the client to consider the response stale immediately.
       },
+      cache: "no-cache",
     });
+
+    const data = await res.json();
+
     if (res.status === 200) {
       return {
         status: true,
         message: "Legal Notes Fetched Successfully",
-        legalNotes: res.data,
+        legalNotes: data,
       };
     } else {
       return { status: false, message: "Failed to load legal notes" };
     }
   } catch (error) {
     const message =
-      error instanceof AxiosError
-        ? error.response
-          ? error.response.data.message
-          : error.message
-        : error instanceof Error
-        ? error.message
-        : "Failed to load legal notes";
+      error instanceof Error ? error.message : "Failed to load legal notes";
     return { status: false, message };
   }
 };
